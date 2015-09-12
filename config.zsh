b1da38bf17321e782cd0ff714ab4aaa078283cbe
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-12 22:02:27 +0200

    fix my-conf array to myconf

--- config.zsh
+++ config.zsh
@@ -1,4 +1,4 @@
-my-conf+=(aliases docker go less llvm ls)
-((OSX)) && my-conf+=(homebrew java locale)
-((LINUX)) && my-conf+=()
-((MSYS)) && my-conf+=()
+myconf+=(aliases docker go less llvm ls)
+((OSX)) && myconf+=(homebrew java locale)
+((LINUX)) && myconf+=()
+((MSYS)) && myconf+=()
