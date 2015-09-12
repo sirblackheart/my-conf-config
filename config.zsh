e43557aab2d4711993007a880b4013db8f982468
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-12 19:07:05 +0200

    remove os-check as own config because it is a hard dependency for my-conf

--- config.zsh
+++ config.zsh
@@ -1,4 +1,4 @@
-my-conf+=(os-check aliases docker go less llvm ls)
+my-conf+=(aliases docker go less llvm ls)
 ((OSX)) && my-conf+=(homebrew java locale)
 ((LINUX)) && my-conf+=()
 ((MSYS)) && my-conf+=()
