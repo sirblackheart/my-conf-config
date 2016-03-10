0c5bc56749aae02593e991e4f977d515eee4ff41
Gabriel Rauter <rauter.gabriel@gmail.com>
2016-03-10 18:39:29 +0100

    add gimp xdg path

--- config.zsh
+++ config.zsh
@@ -1,4 +1,4 @@
-myconf+=(aliases docker go less llvm ls marks)
+myconf+=(aliases docker go less llvm ls marks xdg)
 ((OSX)) && myconf+=(homebrew java locale)
 ((LINUX)) && myconf+=()
 ((MSYS)) && myconf+=()
